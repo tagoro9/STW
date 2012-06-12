1cc02d1a1b5c9179b3f0398e1207e1a0e447d192
pedrojulian <pedro@ubuntu.(none)>
2012-06-12 06:19:46 +0100

    Practica1

---
+++ Practica1/psr.rb
@@ -0,0 +1,43 @@
+require 'sinatra'
+
+before do
+  content_type:html
+  @defeat = {:rock => :paper, :paper => :scissors, :scissors => :rock}
+  @throws = @defeat.keys
+end
+
+get '/' do
+
+  erb:index
+  
+end
+
+
+post '/' do
+  @linea = params[:post]
+   redirect "http://localhost:4567/throw/#{@linea[:opcion]}"
+end
+
+get '/throw/:type' do
+  player_throw = params[:type].to_sym
+  halt 403, "You must throw one of the following: #{@throws}" unless @throws.include? player_throw
+  computer_throw = @throws.sample
+  if player_throw == computer_throw
+    @answer = "you tied with the computer.Try again"
+  elsif computer_throw == @defeat[player_throw]
+    @answer = "you win"
+  else
+    @answer = "Computer win"
+  end
+  erb:salida
+end
+
+
+
+
+
+
+
+
+
+
