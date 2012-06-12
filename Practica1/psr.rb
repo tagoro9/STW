ab7b16b442a51b8f845a78f6527466e6a935333f
pedrojulian <pedro@ubuntu.(none)>
2012-06-12 07:05:34 +0100

    Practica1 version3

--- Practica1/psr.rb
+++ Practica1/psr.rb
@@ -19,12 +19,12 @@ post '/' do
 end
 
 get '/throw/:type' do
-  player_throw = params[:type].to_sym
-  halt 403, "You must throw one of the following: #{@throws}" unless @throws.include? player_throw
-  computer_throw = @throws.sample
-  if player_throw == computer_throw
+  @player_throw = params[:type].to_sym
+  halt 403, "You must throw one of the following: #{@throws}" unless @throws.include? @player_throw
+  @computer_throw = @throws.sample
+  if @player_throw == @computer_throw
     @answer = "You tied with the computer.Try again"
-  elsif computer_throw == @defeat[player_throw]
+  elsif @computer_throw == @defeat[@player_throw]
     @answer = "You win"
   else
     @answer = "Computer win"
