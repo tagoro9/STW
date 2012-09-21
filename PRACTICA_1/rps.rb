5e0c4dd3448b3a48811609b8df39cc44548d46c7
Carlos García <caco85@gmail.com>
2012-09-21 11:52:37 +0100

    practica 1

--- PRACTICA_1/rps.rb
+++ PRACTICA_1/rps.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'erb'
 
 before do
   content_type :html
@@ -11,18 +12,17 @@ get '/' do
 end
 
 post '/' do
-  #@choice = params[:radio]
   redirect "http://localhost:4567/throw/#{params[:radio]}"
 end
 
 get '/throw/:type' do
-  player_throw = params[:type].to_sym
-  halt 403, "You must throw one of the following: #{@throws}" unless @throws.include? player_throw
-  computer_throw = @throws.sample
+  @player_throw = params[:type].to_sym
+  halt 403, "You must throw one of the following: #{@throws}" unless @throws.include? @player_throw
+  @computer_throw = @throws.sample
 
-  if player_throw == computer_throw
+  @answer = if @player_throw == @computer_throw
     "You tied with the computer. Try again."
-  elsif computer_throw == @defeat[player_throw]
+  elsif @computer_throw == @defeat[@player_throw]
     "You win."
   else
     "Computer wins."
