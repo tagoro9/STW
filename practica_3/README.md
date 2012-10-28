e466fc71ed81522763dcfee94f6e60edfeb8199d
Victor <tagoro9@gmail.com>
2012-10-28 11:19:26 +0000

    Practica 3 - Syntaxi

---
+++ practica_3/README.md
@@ -0,0 +1,5 @@
+STW Practica 3 - Servicio de Syntax Highlighting
+=================================================
+
+Aplicación que provee syntax higlighting para un código que se vuelca en un formulario
+
---
+++ practica_3/toopaste.rb
@@ -0,0 +1,29 @@
+require 'sinatra'
+require 'syntaxi'
+
+class String
+	def formatted_body lang
+    	source = "[code lang='#{lang}']
+                #{self}
+              [/code]"
+    	html = Syntaxi.new(source).process
+    	%Q{
+      		<div class="syntax syntax_#{lang}">
+        		#{html}
+      		</div>
+    	}
+  	end	
+end
+
+before do
+	content_type :html
+end
+
+get '/' do
+	erb :new
+end
+
+post '/' do
+	@snippet = params[:body].formatted_body params[:language]
+	erb :show
+end
