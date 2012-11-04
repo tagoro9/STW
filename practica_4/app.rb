beb0ad624f5b820544194653bc9f47619909467e
Victor <tagoro9@gmail.com>
2012-11-04 11:06:36 +0000

    Url shortener started

---
+++ practica_4/app.rb
@@ -0,0 +1,15 @@
+require 'sinatra'
+require 'sinatra/activerecord'
+require 'haml'
+
+class ShortenedUrl < ActiveRecord::Base
+  # Validates whether the value of the specified attributes are unique across the system.
+  validates_uniqueness_of :url
+  # Validates that the specified attributes are not blank
+  validates_presence_of :url
+  #validates_format_of :url, :with => /.*/
+  validates_format_of :url, 
+       :with => %r{^(https?|ftp)://.+}i, 
+       :allow_blank => true, 
+       :message => "The URL must start with http://, https://, or ftp:// ."
+end
