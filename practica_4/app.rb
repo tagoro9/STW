eb4ac387a6955c4c86975b09b7a0b6e502feed79
Victor <tagoro9@gmail.com>
2012-11-04 12:20:11 +0000

    URL shortener version 1.0

--- practica_4/app.rb
+++ practica_4/app.rb
@@ -2,6 +2,9 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require 'haml'
 
+set :database, 'sqlite3:///shortened_urls.db'
+set :address, 'localhost:4567'
+
 class ShortenedUrl < ActiveRecord::Base
   # Validates whether the value of the specified attributes are unique across the system.
   validates_uniqueness_of :url
@@ -12,4 +15,22 @@ class ShortenedUrl < ActiveRecord::Base
        :with => %r{^(https?|ftp)://.+}i, 
        :allow_blank => true, 
        :message => "The URL must start with http://, https://, or ftp:// ."
+end
+
+get '/' do 
+	haml :index, :locals => {:type => ""}
+end
+
+post '/' do
+  @short_url = ShortenedUrl.find_or_create_by_url(params[:url])
+  if @short_url.valid?
+    haml :success, :locals => { :address => settings.address, :type => "success" }
+  else
+    haml :index, :locals => {:type => "error"}
+  end
+end
+
+get '/:shortened' do
+	long_url = ShortenedUrl.find params[:shortened].to_i(36)
+	redirect long_url.url
 end
