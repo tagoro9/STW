a103ea82f7de547f9c9eb08aa79d9d86b379832b
llosimura <alfonsoglezluis@gmail.com>
2012-11-30 21:47:48 +0000

    Practica6 acabada.

--- PRACTICA_6/app.rb
+++ PRACTICA_6/app.rb
@@ -2,83 +2,35 @@
 require 'sinatra'
 set server: 'thin', connections: [], users: Hash.new
 
-
 get '/' do
   halt erb(:login) if params[:user].nil?
   erb :chat, locals: { user: params[:user] }
 end
 
-get '/stream2', provides: 'text/event-stream' do
-  #if users.has_key?(params[:user])
-   # puts "mierda estoy ocupado"
-    #halt erb(:login)
-  #else
-    stream :keep_open do |out|
-      settings.connections << out
-      users[params[:user]] = out
-      out.callback { settings.connections.delete(out) }
-    end
-  #end
-end
+#if users.has_key?(params[:user])
 
 get '/stream/:user', provides: 'text/event-stream' do
   stream :keep_open do |out|
-    settings.connections << out
     settings.users[params[:user]] = out
-    out.callback { settings.connections.delete(out) }
+    out.callback { settings.users.delete params[:user] }
   end
 end
-
 
 post '/' do
   has_nick_expr = /\/(.+):.*/
   nick = has_nick_expr.match(params[:msg])
   connection = settings.users[nick[1]] unless nick.nil?
   if connection.nil?
-    settings.connections.each { |out| out << "data: #{params[:msg]}\n\n" }
+    msg = "#{params[:user]}: #{params[:msg]}"
+    settings.users.each_pair { |user, out| out << "data: #{msg}\n\n" }
     204 # response without entity body
   else
-    connection << "data: #{params[:msg]}\n\n"
-    settings.users[params[:user]] << "data: #{params[:msg]}\n\n" 
+    msg = params[:msg].clone
+    msg.slice! ("/#{nick[1]}:")
+    msg1 = "MP de #{params[:user]} #{msg}"
+    msg2 = "MP para #{nick[1]} #{msg}"
+    connection << "data: #{msg1}\n\n"
+    settings.users[params[:user]] << "data: #{msg2}\n\n" 
+    204 # response without entity body
   end  
 end
-
-__END__
-
-@@ layout
-<html>
-  <head> 
-    <title>Super Simple Chat with Sinatra</title> 
-    <meta charset="utf-8" />
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script> 
-  </head> 
-  <body><%= yield %></body>
-</html>
-
-@@ login
-<form action='/'>
-  <label for='user'>User Name:</label>
-  <input name='user' value='' />
-  <input type='submit' value="GO IN THE CHAT!" />
-</form>
-
-@@ chat
-<pre id='chat'></pre>
-
-<script>
-  // reading
-  var url = "/stream/" + "<%= user %>"
-  var es = new EventSource(url);
-  es.onmessage = function(e) { $('#chat').append(e.data + "\n") };
-
-  // writing
-  $("form").live("submit", function(e) {
-    $.post('/', {msg: "<%= user %>: " + $('#msg').val(), user: "<%= user %>" });
-    $('#msg').val(''); $('#msg').focus();
-    e.preventDefault();
-  });
-</script>
-
-<form>
-  <input id='msg' placeholder='type message here...' />
-</form>
