ee1ade3789529e0f3f67a4582857a561826ed79e
Jonay <jhonsay@gmail.com>
2012-12-01 11:30:04 +0000

    Prac05

--- Prac05/db/migrate/20121201095136_add_custom_to_shortened_urls.rb
+++
@@ -1,9 +0,0 @@
-class AddCustomToShortenedUrls < ActiveRecord::Migration
-  def up
-     add_column :shortened_urls, :custom, :string
-  end
-
-  def down
-     remove_column :shortened_urls, :custom
-  end
-end
