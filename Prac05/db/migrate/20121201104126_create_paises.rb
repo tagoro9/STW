cfe07e12b57a968310d80d12894933d5aafa8467
Jonay <jhonsay@gmail.com>
2012-12-01 11:02:32 +0000

    Praco05

---
+++ Prac05/db/migrate/20121201104126_create_paises.rb
@@ -0,0 +1,14 @@
+class CreatePaises < ActiveRecord::Migration
+  def up
+    create_table :paises do |t|
+      t.string :url
+      t.string :pais
+      t.integer :visitas, :default => 0
+    end
+    add_index :paises, :url
+  end
+ 
+  def down
+    drop_table :paises
+  end
+end
