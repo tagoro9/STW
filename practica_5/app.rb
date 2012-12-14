40d54c7caa70c110294786fd62e048cdfe779ab8
Victor <tagoro9@gmail.com>
2012-12-14 00:44:29 +0000

    chat funcionando con estilos

--- practica_5/app.rb
+++ practica_5/app.rb
@@ -1,63 +1,43 @@
 # coding: utf-8
 require 'sinatra'
-set server: 'thin', connections: []
+require 'json'
+set server: 'thin', users: {}
+
+class String
+  def sanitize
+    self.gsub(/\W/,'')
+  end
+end
 
 get '/' do
   halt erb(:login) unless params[:user]
-  erb :chat, locals: { user: params[:user].gsub(/\W/, '') }
+  erb :chat, locals: { user: params[:user].sanitize }
 end
 
-get '/stream', provides: 'text/event-stream' do
+get '/check/:user' do #Check if :name is already taken
+   settings.users.keys.include?(params[:user].sanitize)  == true ?  JSON.generate({:error => "Name already taken"}) : JSON.generate({:success => "Welcome aboard!"})
+end
+
+get '/stream/:user', provides: 'text/event-stream' do
   stream :keep_open do |out|
-    settings.connections << out
-    out.callback { settings.connections.delete(out) }
+    out << "data: #{JSON.generate({:users => settings.users.keys})}\n\n" #Send all users list to the new user
+    settings.users.values.each { |out| out << "data: #{JSON.generate({:joined => params[:user]})}\n\n" } #Notify the user joined to other users
+    settings.users[params[:user]] = out #Save new user's connection
+    out.callback { 
+      name = settings.users.key out
+      settings.users.delete name
+      settings.users.values.each { |out| out << "data: #{JSON.generate({:left => name})}\n\n" } #Notify the user left to other users
+    }
   end
 end
 
 post '/' do
-  puts "RECEIVED NEW MESSAGE!!!!!! #{params[:msg]}"
-  settings.connections.each { |out| out << "data: #{params[:msg]}\n\n" }
+  to = /\/(.+):(.*)/.match(params[:msg])
+  to,msg = to[1], to[2] unless to.nil?
+  if (defined? to) and (out = settings.users[to]) != nil
+    out << "data: #{JSON.generate({:message => msg, :user => params[:user], :private => true})}\n\n"    
+  else
+    settings.users.values.each { |out| out << "data: #{JSON.generate({:message => params[:msg], :user => params[:user]})}\n\n" } #Broadcast message
+  end
   204 # response without entity body
 end
-
-__END__
-
-@@ layout
-<html>
-  <head> 
-    <title>Super Simple Chat with Sinatra</title> 
-    <meta charset="utf-8" />
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script> 
-  </head> 
-  <body><%= yield %></body>
-</html>
-
-@@ login
-<form action='/'>
-  <label for='user'>User Name:</label>
-  <input name='user' value='' />
-  <input type='submit' value="GO!" />
-</form>
-
-@@ chat
-<pre id='chat'></pre>
-
-<script>
-  // reading
-  var es = new EventSource('/stream');
-  es.onmessage = function(e) { 
-    console.log("New message!")
-    $('#chat').append(e.data + "\n")
-   };
-
-  // writing
-  $("form").live("submit", function(e) {
-    $.post('/', {msg: "<%= user %>: " + $('#msg').val()});
-    $('#msg').val(''); $('#msg').focus();
-    e.preventDefault();
-  });
-</script>
-
-<form>
-  <input id='msg' name="msg" placeholder='type message here...' />
-</form>
